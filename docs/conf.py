4e3eed9df3c2e49a4bf07103cea9a2b810f3f7b8
Philippe Morisseau <p.morisseau@groupeonepoint.com>
2025-10-09 10:56:55 +0200

    Ajout du thème sphinx_rtd_theme pour la documentation Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx'
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
